550ff1abe3e657e7e757a8daa84a8ee9818127c0
mahmoud0207 <mahmoudsalem0207@gmail.com>
2023-06-05 23:04:41 +0300

    upload

---
+++ resources/views/frontend/About.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.customer')
+
+
+@section('title')
+   Pain & Gain
+@endsection
+
+
+@section('content')
+<div class="py-5"></div>
+<div class="container p-2 my-2">
+        <h1 class="p-2">WHO WE ARE</h1>
+        <p>We get it. You don’t want to be ordinary. That’s why you’re here.
+            
+            We believe in the dreamers but more importantly, the doers. We’ll provide the tools, you put in the work.
+            
+            Together, we can create healthier and longer lives. Let’s get to work.
+            
+            Build your body. Build your mind. Build your life.</p>
+    </div>
+<div class="py-5"></div>
+@endsection
